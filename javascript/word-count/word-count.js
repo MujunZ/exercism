f928709747015f965eed9bbb945275b362e1fded
MujunZ <o.6180339@gmail.com>
2017-03-02 11:16:26 -0500

    Solved all, but the Regex is not good

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -2,28 +2,24 @@ var Words = function () {
 };
 
 Words.prototype.count = function(input) {
-	var count = {};
+	//var count;
 	// validate input
 	if (typeof input === 'string') {
 		// split input to array
-		//var rawWords = input.split(/[^A-Za-z0-9_']/);
-		var rawWords = input.split(/[^\w']/);
-		// clean the empty elements in the array
+		var rawWords = input.toLowerCase().split(/[\s:.,;"!&@$%^&_#*~?¡¿]/);
 		var words = rawWords.filter(Boolean);
-		// clean the apostrophes. ???? is there a more elegant way to do it in the rawword step?????
 		for(var i = 0, length1 = words.length; i < length1; i++){
 			words[i] = words[i].split(/^'|'$/).filter(Boolean).join();
 		}
-		// loop words array 
-		for(var i = 0, length1 = words.length; i < length1; i++){
-			words[i] = words[i].toLowerCase();
-			if (typeof count[words[i]] === 'undefined') {
-				count[words[i]] = 1;
-			}else{
-				count[words[i]]++;
-			};	
-		};
-		return count;
+		var ct = words.reduce(function (count,word) {
+			if (count.hasOwnProperty(word)) {
+				count[word]++;
+			} else {
+				count[word] = 1;
+			}
+			return count;
+		}, {});
+	return ct;
 	} else {
 		throw new Error('Input is not a String.')
 	}
