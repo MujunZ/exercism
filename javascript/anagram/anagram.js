60533a2847b0420b0b8792e15c7ef28cf4a90014
MujunZ <o.6180339@gmail.com>
2017-03-04 20:10:51 -0500

    Use function. Be careful of 'this'

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,11 +3,12 @@ var Anagram = function (subject) {
 };
 
 Anagram.prototype.matches = function(...wordList) {
-	this.matchWords = [];
+	var matchWords = [];
+	var subject = this.subject;
 	function matchWord (word) {
-		console.log(word);
-		if (word.toLowerCase().split('').sort().join('') === this.subject.split('').sort().join('') && word.toLowerCase() !== this.subject) {
-		 	this.matchWords.push(word);
+		var tempWords = word.toLowerCase();
+		if (tempWords.split('').sort().join('') === subject.split('').sort().join('') && word.toLowerCase() !== subject) {
+		 	matchWords.push(word);
 		 } 
 	}
 	//wordList length = 1?
@@ -16,24 +17,17 @@ Anagram.prototype.matches = function(...wordList) {
 		if (typeof wordList[0] === 'object') {
 			//for wordList[0].length, sort each one and join, compare with subject
 			for(var i = 0, length1 = wordList[0].length; i < length1; i++){
-				if (wordList[0][i].toLowerCase().split('').sort().join('') === this.subject.split('').sort().join('') && wordList[0][i].toLowerCase() !== this.subject) {
-					this.matchWords.push(wordList[0][i]);
-				};				
+				matchWord(wordList[0][i]);			
 			};
 		} else if (typeof wordList[0] === 'string') {
-			//matchWord(wordList[0]); Why can't I use this function here? it always return 'split is undefined'
-			if (wordList[0].toLowerCase().split('').sort().join('') === this.subject.split('').sort().join('') && wordList[0].toLowerCase() != this.subject) {
-				this.matchWords.push(wordList[0]);
-			}
+			matchWord(wordList[0]);
 		}
 	} else if (wordList.length > 1) {
 		for(var i = 0, length1 = wordList.length; i < length1; i++){
-				if (wordList[i].toLowerCase().split('').sort().join('') === this.subject.split('').sort().join('') && wordList[i].toLowerCase() !== this.subject) {
-					this.matchWords.push(wordList[i]);
-				};				
+				matchWord(wordList[i]);			
 			};
 	}	
-	return this.matchWords;
+	return matchWords;
 };
 
 module.exports = Anagram;
