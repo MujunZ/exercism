78c91d10822238bccaabe690fe301eac1809a152
MujunZ <o.6180339@gmail.com>
2017-03-08 23:22:32 -0500

    initiate

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,12 @@
+var FoodChain = function () {
+	this.verses = [
+		'I know an old lady who swallowed a fly.\n I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
+		'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n',
+		'I know an old lady who swallowed a bird.\nHow absurd to swallow a bird!\n',
+		
+	]
+}
+
+FoodChain.prototype.verse = function(first_argument) {
+	// body...
+};
